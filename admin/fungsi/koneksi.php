eaeab7415f7c75a0ca410d0cba670fd12f4d9c88
rezir008 <rezir008@gmail.com>
2020-12-16 20:34:03 +0800

    koneksi

--- admin/fungsi/koneksi.php
+++ admin/fungsi/koneksi.php
@@ -1,8 +1,3 @@
 <?php 
-try { 
-    $pdo_conn = new PDO('mysql:host=localhost;dbname=lmmc', 'root', '',
-    array(PDO::ATTR_PERSISTENT => true)); 
-} 
-catch(PDOException $e) { echo $e->getMessage(); 
-} 
+include("../koneksi.php");
 ?>
