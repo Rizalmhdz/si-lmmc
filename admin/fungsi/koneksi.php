aedb52723273bf5db837b30ff0b045a1d9d122c2
Rizalmhmmd13zx <rizalmhmmd.13zx@gmail.com>
2020-12-16 21:40:33 +0800

    update koneksi.php untuk remote sql

--- admin/fungsi/koneksi.php
+++ admin/fungsi/koneksi.php
@@ -1,8 +1,22 @@
-<?php 
-try { 
-    $pdo_conn = new PDO('mysql:host=localhost;dbname=lmmc', 'root', '',
-    array(PDO::ATTR_PERSISTENT => true)); 
-} 
-catch(PDOException $e) { echo $e->getMessage(); 
-} 
+<?php
+// $db_host="localhost"; //localhost server 
+// $db_user="root"; //database username
+// $db_password=""; //database password   
+// $db_name="lmmc"; //database name
+
+$db_host="remotemysql.com"; //localhost server 
+$db_user="M22KGQDNEI"; //database username
+$db_password="SRko0GCJ6c"; //database password   
+$db_name="M22KGQDNEI"; //database name
+
+try
+{
+ $pdo_conn=new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_password);
+ $pdo_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOEXCEPTION $e)
+{
+ $e->getMessage();
+}
+
 ?>
--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,13 @@
 <?php
-$db_host="localhost"; //localhost server 
-$db_user="root"; //database username
-$db_password=""; //database password   
-$db_name="lmmc"; //database name
+// $db_host="localhost"; //localhost server 
+// $db_user="root"; //database username
+// $db_password=""; //database password   
+// $db_name="lmmc"; //database name
+
+$db_host="remotemysql.com"; //localhost server 
+$db_user="M22KGQDNEI"; //database username
+$db_password="SRko0GCJ6c"; //database password   
+$db_name="M22KGQDNEI"; //database name
 
 try
 {
